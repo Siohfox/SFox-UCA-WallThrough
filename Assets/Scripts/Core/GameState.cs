ae39c7e8e932700a87416370e5c0800cd1232b06
Siohfox <72400408+Siohfox@users.noreply.github.com>
2024-10-08 23:38:59 +0100

    Minor core changes

--- Assets/Scripts/Core/GameState.cs
+++ Assets/Scripts/Core/GameState.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameState : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace WallThrough.Core
+{ 
+    public class GameState : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
+
