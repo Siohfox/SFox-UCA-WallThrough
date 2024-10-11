70252520fb1d5f2ee0d0c7e9b19b2b968014ed32
Siohfox <72400408+Siohfox@users.noreply.github.com>
2024-10-11 04:59:08 +0100

    Added advice text

--- Assets/Scripts/Gameplay/EndPortal.cs
+++ Assets/Scripts/Gameplay/EndPortal.cs
@@ -10,6 +10,8 @@ namespace WallThrough.Gameplay
     {
         [SerializeField]
         private GameObject winText;
+        [SerializeField]
+        private GameObject adviceText;
         private ObjectiveManager objectiveManager;
 
         private void Start()
@@ -21,11 +23,20 @@ namespace WallThrough.Gameplay
         {
             if (objectiveManager.CheckObjectives())
                 winText.SetActive(true);
+            else
+                StartCoroutine(GiveAdvice());
         }
 
         public void InteractionEnd()
         {
             // stuff
+        }
+
+        private IEnumerator GiveAdvice()
+        {
+            adviceText.SetActive(true);
+            yield return new WaitForSeconds(2f);
+            adviceText.SetActive(false);
         }
     }
 }
