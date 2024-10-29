931cc2fe161cf5fe8a52c56fd7191c3d24d064d4
1911666 Ross Bates <1911666@students.ucreative.ac.uk>
2024-10-29 12:26:02 +0000

    Making tutorial level

--- Assets/Scripts/Gameplay/Player/Movement.cs
+++ Assets/Scripts/Gameplay/Player/Movement.cs
@@ -2,54 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-public class Movement : MonoBehaviour
+namespace WallThrough.Gameplay.Pawn
 {
-    private Rigidbody _rb;
-
-    public float moveSpeed = 5f;
-    public float acceleration = 10f;
-    public float deceleration = 10f;
-    public float rotationSpeed = 720f; // Degrees per second
-    private Vector3 currentVelocity;
-
-    private void Start()
+    [RequireComponent(typeof(Rigidbody))]
+    public class Movement : MonoBehaviour
     {
-        _rb = GetComponent<Rigidbody>();
-        currentVelocity = Vector3.zero; // Start with no movement
-    }
+        private Rigidbody _rb;
 
-    public void Move(Vector2 moveDir)
-    {
-        // Calculate target velocity based on input
-        Vector3 targetVelocity = new Vector3(moveDir.x, 0, moveDir.y).normalized * moveSpeed;
+        public float moveSpeed = 5f;
+        public float acceleration = 10f;
+        public float deceleration = 10f;
+        public float rotationSpeed = 720f; // Degrees per second
+        private Vector3 currentVelocity;
 
-        // Smoothly transition to the target velocity using acceleration/deceleration
-        if (targetVelocity.magnitude > 0)
+        private void Start()
         {
-            // Accelerate towards the target velocity
-            currentVelocity = Vector3.MoveTowards(currentVelocity, targetVelocity, acceleration * Time.fixedDeltaTime);
-            RotateTowards(currentVelocity);
-        }
-        else
-        {
-            // If no input, decelerate to zero
-            currentVelocity = Vector3.MoveTowards(currentVelocity, Vector3.zero, deceleration * Time.fixedDeltaTime);
+            _rb = GetComponent<Rigidbody>();
+            currentVelocity = Vector3.zero; // Start with no movement
         }
 
-        // Apply the calculated velocity to the Rigidbody
-        _rb.velocity = new Vector3(currentVelocity.x, _rb.velocity.y, currentVelocity.z);
-    }
-
-    private void RotateTowards(Vector3 direction)
-    {
-        if (direction.magnitude > 0)
+        public void Move(Vector2 moveDir)
         {
-            // Calculate the target rotation
-            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            // Calculate target velocity based on input
+            Vector3 targetVelocity = new Vector3(moveDir.x, 0, moveDir.y).normalized * moveSpeed;
 
-            // Smoothly rotate towards the target rotation
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+            // Smoothly transition to the target velocity using acceleration/deceleration
+            if (targetVelocity.magnitude > 0)
+            {
+                // Accelerate towards the target velocity
+                currentVelocity = Vector3.MoveTowards(currentVelocity, targetVelocity, acceleration * Time.fixedDeltaTime);
+                RotateTowards(currentVelocity);
+            }
+            else
+            {
+                // If no input, decelerate to zero
+                currentVelocity = Vector3.MoveTowards(currentVelocity, Vector3.zero, deceleration * Time.fixedDeltaTime);
+            }
+
+            // Apply the calculated velocity to the Rigidbody
+            _rb.velocity = new Vector3(currentVelocity.x, _rb.velocity.y, currentVelocity.z);
         }
+
+        private void RotateTowards(Vector3 direction)
+        {
+            if (direction.magnitude > 0)
+            {
+                // Calculate the target rotation
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+                // Smoothly rotate towards the target rotation
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+            }
+        }
+
+        public Vector3 GetCurrentVelocity() => currentVelocity;
     }
 }
---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using WallThrough.Gameplay.Pawn;
+
+public class Tutorial : MonoBehaviour
+{
+    public enum TutorialState
+    {
+        WASD,
+        FindColour,
+        WalkNearDoor,
+        CollectCollectables,
+        Exit,
+        Neutral
+    }
+
+    [SerializeField] private TMP_Text WASDText;
+    [SerializeField] private TMP_Text findTheColourCodeText;
+    [SerializeField] private TMP_Text walkNearDoorText;
+    [SerializeField] private TMP_Text collectCollectableText;
+    [SerializeField] private TMP_Text ExitPortalText;
+
+    [SerializeField] private TutorialState state;
+
+    [SerializeField] private Movement playerMovement;
+
+    private void Start()
+    {
+        state = TutorialState.WASD; 
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case TutorialState.WASD:
+                WASDText.gameObject.SetActive(true);     
+                if(playerMovement.GetCurrentVelocity().x > 0 || playerMovement.GetCurrentVelocity().z > 0)
+                {
+                    Debug.Log("Player moving");
+                    WASDText.gameObject.SetActive(false);
+                    state = TutorialState.FindColour;
+                }
+                break;
+            case TutorialState.FindColour:
+                StartCoroutine(FlashText(findTheColourCodeText.gameObject, 4));
+                state = TutorialState.Neutral;
+                break;
+            case TutorialState.WalkNearDoor:
+                StartCoroutine(FlashText(walkNearDoorText.gameObject, 2));
+                state = TutorialState.Neutral;
+                break;
+            case TutorialState.CollectCollectables:
+                StartCoroutine(FlashText(collectCollectableText.gameObject, 2));
+                state = TutorialState.Neutral;
+                break;
+            case TutorialState.Exit:
+                StartCoroutine(FlashText(ExitPortalText.gameObject, 2));
+                state = TutorialState.Neutral;
+                break;
+            case TutorialState.Neutral:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private IEnumerator FlashText(GameObject textGameobject, float timeToWait)
+    {
+        textGameobject.SetActive(true);
+        yield return new WaitForSeconds(timeToWait);
+        textGameobject.SetActive(false);
+    }
+}
