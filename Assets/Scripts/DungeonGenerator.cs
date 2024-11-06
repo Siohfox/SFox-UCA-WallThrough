608c0a6fca4a6ecb5de8ad34858dddeecfa0de8a
Siohfox <72400408+Siohfox@users.noreply.github.com>
2024-11-06 23:26:16 +0000

    Updated dungeon generation logic

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -14,10 +14,34 @@ public class DungeonGenerator : MonoBehaviour
 
     public Vector2 generationSize;
     public int startPos = 0;
-    public GameObject room;
+    public GameObject finalBoss;
     public Vector2 offset;
+    public Rule[] rooms;
+    List<Cell> dungeonGrid;
 
-    List<Cell> board;
+    [System.Serializable]
+    public class Rule
+    {
+        public GameObject room;
+        public Vector2Int minPosition;
+        public Vector2Int maxPosition;
+        
+
+        public bool obligatory;
+
+        public int ProbabilityOfSpawning(int x, int y)
+        {
+            // 0 - cannot spawn 1 - can spawn 2 - HAS to spawn
+
+            if (x >= minPosition.x && x <= maxPosition.x && y >= minPosition.y && y <= maxPosition.y)
+            {
+                return obligatory ? 2 : 1;
+            }
+
+            return 0;
+        }
+
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -32,26 +56,56 @@ public class DungeonGenerator : MonoBehaviour
         {
             for (int j = 0; j < generationSize.y; j++)
             {
-                Cell currentCell = board[Mathf.FloorToInt(i + j * generationSize.x)];
-
+                Cell currentCell = dungeonGrid[Mathf.FloorToInt(i + j * generationSize.x)];
                 if (currentCell.visited)
                 {
-                    var newRoom = Instantiate(room, new Vector3(i * offset.x, 0, -j * offset.y), quaternion.identity, transform).GetComponent<RoomBehaviour>();
-                    newRoom.UpdateRoom(currentCell.status);
-                    newRoom.name += " " + i + " " + j;
-                }
+                    int randomRoom = -1;
+                    List<int> availableRooms = new List<int>();
 
-                // Wait for 1 second before continuing to the next room
-                yield return new WaitForSeconds(0.3f);
+                    for (int k = 0; k < rooms.Length; k++)
+                    {
+                        int p = rooms[k].ProbabilityOfSpawning(i, j);
+
+                        if (p == 2)
+                        {
+                            randomRoom = k;
+                            break;
+                        }
+                        else if (p == 1)
+                        {
+                            availableRooms.Add(k);
+                        }
+                    }
+
+                    if (randomRoom == -1)
+                    {
+                        if (availableRooms.Count > 0)
+                        {
+                            randomRoom = availableRooms[UnityEngine.Random.Range(0, availableRooms.Count)];
+                        }
+                        else
+                        {
+                            randomRoom = 0;
+                        }
+                    }
+
+
+                    var newRoom = Instantiate(rooms[randomRoom].room, new Vector3(i * offset.x, 0, -j * offset.y), Quaternion.identity, transform).GetComponent<RoomBehaviour>();
+                    newRoom.UpdateRoom(currentCell.status);
+                    newRoom.name += " " + i + "-" + j;
+
+
+                    yield return new WaitForSeconds(0.3f);
+                }
             }
         }
 
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.3f);
     }
 
     void MazeGenerator()
     {
-        board = new List<Cell>();
+        dungeonGrid = new List<Cell>();
 
         if (generationSize.x < 1 || generationSize.y < 1)
         {
@@ -63,7 +117,7 @@ public class DungeonGenerator : MonoBehaviour
         {
             for (int j = 0; j < generationSize.y; j++)
             {
-                board.Add(new Cell());
+                dungeonGrid.Add(new Cell());
             }
         }
 
@@ -75,9 +129,9 @@ public class DungeonGenerator : MonoBehaviour
         {
             k++;
 
-            board[currentCell].visited = true;
+            dungeonGrid[currentCell].visited = true;
 
-            if (currentCell == board.Count - 1)
+            if (currentCell == dungeonGrid.Count - 1)
             {
                 break;
             }
@@ -87,9 +141,8 @@ public class DungeonGenerator : MonoBehaviour
                 Debug.LogError("Invalid size");
                 return;
             }
-            board[currentCell].visited = true;
 
-            if (currentCell == board.Count - 1)
+            if (currentCell == dungeonGrid.Count - 1)
             {
                 break;
             }
@@ -119,15 +172,15 @@ public class DungeonGenerator : MonoBehaviour
                     // down or right
                     if (newCell - 1 == currentCell)
                     {
-                        board[currentCell].status[2] = true;
+                        dungeonGrid[currentCell].status[2] = true;
                         currentCell = newCell;
-                        board[currentCell].status[3] = true;
+                        dungeonGrid[currentCell].status[3] = true;
                     }
                     else
                     {
-                        board[currentCell].status[1] = true;
+                        dungeonGrid[currentCell].status[1] = true;
                         currentCell = newCell;
-                        board[currentCell].status[0] = true;
+                        dungeonGrid[currentCell].status[0] = true;
                     }
                 }
                 else
@@ -135,15 +188,15 @@ public class DungeonGenerator : MonoBehaviour
                     // up or left
                     if (newCell + 1 == currentCell)
                     {
-                        board[currentCell].status[3] = true;
+                        dungeonGrid[currentCell].status[3] = true;
                         currentCell = newCell;
-                        board[currentCell].status[2] = true;
+                        dungeonGrid[currentCell].status[2] = true;
                     }
                     else
                     {
-                        board[currentCell].status[0] = true;
+                        dungeonGrid[currentCell].status[0] = true;
                         currentCell = newCell;
-                        board[currentCell].status[1] = true;
+                        dungeonGrid[currentCell].status[1] = true;
                     }
                 }
             }
@@ -151,6 +204,9 @@ public class DungeonGenerator : MonoBehaviour
 
         // Start generating the dungeon with delays
         StartCoroutine(GenerateDungeon());
+
+        // After the dungeon is generated, place the boss in the final room
+        PlaceFinalBoss();
     }
 
     List<int> CheckNeighbors(int cell)
@@ -158,29 +214,48 @@ public class DungeonGenerator : MonoBehaviour
         List<int> neighbors = new List<int>();
 
         // check up neighbor
-        if (cell - generationSize.x >= 0 && !board[Mathf.FloorToInt(cell - generationSize.x)].visited)
+        if (cell - generationSize.x >= 0 && !dungeonGrid[Mathf.FloorToInt(cell - generationSize.x)].visited)
         {
             neighbors.Add(Mathf.FloorToInt(cell - generationSize.x));
         }
 
         // check down neighbor
-        if (cell + generationSize.x < board.Count && !board[Mathf.FloorToInt(cell + generationSize.x)].visited)
+        if (cell + generationSize.x < dungeonGrid.Count && !dungeonGrid[Mathf.FloorToInt(cell + generationSize.x)].visited)
         {
             neighbors.Add(Mathf.FloorToInt(cell + generationSize.x));
         }
 
         // check right neighbor
-        if ((cell + 1) % generationSize.x != 0 && !board[(cell + 1)].visited)
+        if ((cell + 1) % generationSize.x != 0 && !dungeonGrid[(cell + 1)].visited)
         {
             neighbors.Add((cell + 1));
         }
 
         // check left neighbor
-        if (cell % generationSize.x != 0 && !board[(cell - 1)].visited)
+        if (cell % generationSize.x != 0 && !dungeonGrid[(cell - 1)].visited)
         {
             neighbors.Add((cell - 1));
         }
 
         return neighbors;
+    }
+
+    public Vector2 GetRoomCoordinates(int index)
+    {
+        int x = index % (int)generationSize.x;
+        int y = index / (int)generationSize.x;
+        return new Vector2(x, y);
+    }
+
+
+    void PlaceFinalBoss()
+    {
+        // Get coordinates of the final room
+        int finalRoomIndex = dungeonGrid.Count - 1;
+        Vector2 finalRoomCoordinates = GetRoomCoordinates(finalRoomIndex);
+
+        // Instantiate the boss in the middle of the final room
+        Vector3 bossPosition = new Vector3(finalRoomCoordinates.x * offset.x + offset.x / 2, 0, -finalRoomCoordinates.y * offset.y - offset.y / 2);
+        Instantiate(finalBoss, bossPosition, Quaternion.identity, transform);
     }
 }
